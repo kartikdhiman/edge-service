0caf692aca4d9423eefc3493bac94347be40a1ee
kartikdhiman <59189590+kartikdhiman@users.noreply.github.com>
2024-07-12 22:38:33 +0530

    test: update test with dependencies

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,30 +2,28 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest(
-				webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 class EdgeServiceApplicationTests {
 
 	private static final int REDIS_PORT = 6379;
 
 	@Container
+	@ServiceConnection(name = "redis")
 	static GenericContainer<?> redis =
-					new GenericContainer<>(DockerImageName.parse("redis:7.2-alpine"))
+					new GenericContainer<>("redis:7.2-alpine")
 									.withExposedPorts(REDIS_PORT);
 
-	@DynamicPropertySource
-	static void redisProperties(DynamicPropertyRegistry registry) {
-		registry.add("spring.data.redis.host", () -> redis.getHost());
-		registry.add("spring.data.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+	@Test
+	void connectionEstablished() {
+		assertThat(redis.isRunning()).isTrue();
 	}
 
 	@Test
